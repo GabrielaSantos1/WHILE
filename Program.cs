5fd25807d28fa8fec2d52be56fd813f81a2ec40b
GabrielaSantos1 <104846859+GabrielaSantos1@users.noreply.github.com>
2023-06-05 21:31:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Programa
+{
+    static void Main()
+    {
+        int num, i = 1;
+
+        Console.WriteLine("Digite um número inteiro positivo: ");
+        num = int.Parse(Console.ReadLine());
+
+
+        while (i <= num)
+        {
+
+            Console.WriteLine( i + " "); i++;
+
+        }
+    }
+
+}
